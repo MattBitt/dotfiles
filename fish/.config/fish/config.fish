cd0b35fb5a73c702a85c1d85a7ea70de58f6eadc
MattBitt <mattsbitt@gmail.com>
2025-10-25 12:07:11 -0500

    removing bad git alias

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    function git
-        set project_root /home/matt/programming/hmtc
-        set frontend_dir $project_root/frontend
-
-        # If current directory is frontend or any subdirectory of frontend
-        if string match -q "$frontend_dir*" (pwd)
-            echo "Don't run git in frontend/. Redirecting to project root..."
-            pushd $project_root > /dev/null
-            command git $argv
-            popd > /dev/null
-            return $status
-        end
-        command git $argv
-   end
+   # Commands to run in interactive sessions can go here
 end
+
 export PATH="$HOME/bin:$PATH"
 alias run="./run"
-
-alias claude="/home/matt/.claude/local/claude"
